24d675eca7d27b9ae2ca9ac3bb6e641d6efe8591
NoRoboDude <2023sagi.n@ebin.ort.org.il>
2023-03-07 15:28:48 +0200

    test commit Removed deez nutz

--- Test.py
+++ Test.py
@@ -5,6 +5,5 @@ vehicle.armed = True
 vehicle.mode = VehicleMode("GUIDED")
 vehicle.simple_takeoff(alt=10)
 vehicle.wait_for_alt(10, epsilon=0.1 , rel=True, timeout=None)
-a_location = LocationGlobalRelative(-35.367114, 149.196022, 0)
+a_location = LocationGlobalRelative(-35.363114, 149.166022, 0)
 vehicle.simple_goto(a_location)
-print("deez nutz")
